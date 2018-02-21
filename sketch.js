d27ad7de19ca05a3b92d691f315ed95578224148
Shubham Kushwah <shubhamkushwahsk@gmail.com>
2018-02-21 20:10:44 +0530

    Added color changes while moving

--- sketch.js
+++ sketch.js
@@ -1,22 +1,43 @@
 var angle = 0;
 var slider;
 var stroke_val = 8;
+// var color = 255;
 
 function setup() {
-	createCanvas(1000, 700);
+	createCanvas(1200, 840);
     slider = createSlider(0, TWO_PI, PI/4, 0.01);
 }
 
+var colour = 255;
+var inc = 0;
+var above = true;
+
 function draw() {
-    angle = slider.value()
+    angle = slider.value();
+    var height_ = height;
+    // angle += 0.05;
+    col1 = (colour-angle*30)%255;
+    col2 = (colour+angle*30)%255;
+    col3 = (colour-angle*15)%255;
+    stroke(col1, col2, col3);
 	background(51);
     strokeWeight(stroke_val);
-    translate(500, height); //shifting the origin to bottom-mid
-    branch(175, stroke_val);
+    translate(600, height_ - inc); //shifting the origin to bottom-mid
+    branch(250 - inc, stroke_val);
+    // if(inc < 250 && above) {
+    //     inc += 1;
+    // }
+    // else if(inc >= 250 || !above) {
+    //     inc -= 1;
+    //     above = false;
+    //     if(inc == 0) {
+    //         above = true;
+    //     }
+    // }
 }
 
 function branch(len, s_val) {
-    stroke(255);
+    // stroke(255);
     strokeWeight(s_val);
     line(0, 0, 0, -len);
     translate(0, -len);
@@ -24,11 +45,11 @@ function branch(len, s_val) {
         
         push();
         rotate(angle);
-        branch(len*0.67, s_val*0.8);
+        branch(len*0.7, s_val*0.8);
         pop();
         push();
         rotate(-angle);
-        branch(len*0.67, s_val*0.8);
+        branch(len*0.7, s_val*0.8);
         pop();
     }
 }
